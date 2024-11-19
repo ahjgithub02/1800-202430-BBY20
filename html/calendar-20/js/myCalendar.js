39f3cb57459507717bf58de795cc763250ae951e
Brownie Tran <tknguyen2111@gmail.com>
2024-11-19 10:04:05 -0800

    move canlendar to html folder

--- html/calendar-20/js/myCalendar.js
+++ html/calendar-20/js/myCalendar.js
@@ -1,20 +1,20 @@
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCr-tFeRpREksBAuds5SV3hHJeDoBrTYjs",
-//     authDomain: "remindme-ce614.firebaseapp.com",
-//     projectId: "remindme-ce614",
-//     storageBucket: "remindme-ce614.firebasestorage.app",
-//     messagingSenderId: "464351423320",
-//     appId: "1:464351423320:web:af0ee3b8ab0070cc55132d"
-//   };
+const firebaseConfig = {
+    apiKey: "AIzaSyCr-tFeRpREksBAuds5SV3hHJeDoBrTYjs",
+    authDomain: "remindme-ce614.firebaseapp.com",
+    projectId: "remindme-ce614",
+    storageBucket: "remindme-ce614.firebasestorage.app",
+    messagingSenderId: "464351423320",
+    appId: "1:464351423320:web:af0ee3b8ab0070cc55132d"
+  };
   
 //--------------------------------------------
 // initialize the Firebase app
 // initialize Firestore database if using it
 //--------------------------------------------
-// const app = firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
-// const storage = firebase.storage();
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();
 
 
 document.addEventListener('DOMContentLoaded', function() {
