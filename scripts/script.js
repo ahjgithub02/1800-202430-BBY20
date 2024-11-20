7ce36e598439f05f5394a706095ca3795f8c0111
ahjgithub02 <hwanga01@gmail.com>
2024-11-20 14:00:01 -0800

    made changes to main.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,25 @@
-function sayHello() {
-    
+// Function to handle checkbox change event and show the notification
+function notifyTaskCompletion(event) {
+    const checkbox = event.target;
+
+    if (checkbox.checked) {
+        console.log("Task completed!");
+
+        const toast = new bootstrap.Toast(document.getElementById('taskNotification'));
+        toast.show();
+    }
 }
-//sayHello();
+
+// Add the event listeners to the checkboxes
+document.querySelectorAll('.form-check-input').forEach(checkbox => {
+    checkbox.addEventListener('click', notifyTaskCompletion);
+});
+
+document.getElementById("toggleButton").addEventListener("click", function() {
+    var textElement = document.getElementById("toggleText");
+    if (textElement.style.display === "none") {
+      textElement.style.display = "block"; // Show the text
+    } else {
+      textElement.style.display = "none"; // Hide the text
+    }
+  });
