f58451fbd8191ab5d7ec846330ec208f8aebc3aa
ahjgithub02 <hwanga01@gmail.com>
2024-11-24 17:35:38 -0800

    cleaning up index page html

---
+++ scripts/smooth-scroll.js
@@ -0,0 +1,24 @@
+// Smooth scroll with an offset to account for the fixed navbar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        // Special handling for the #top link
+        if (targetId === 'top') {
+            window.scrollTo({
+                // Scrolls to very top of the page when clicking RemindMe Logo
+                top: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            window.scrollTo({
+                // Adjust 65px offset based on navbar height
+                top: targetElement.offsetTop - 65,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
