b59cf5ee8bd897aa369d8b9cfe0ba316ae72d2b0
Brownie Tran <tknguyen2111@gmail.com>
2024-12-06 14:17:02 -0800

    Fix small bug in the sign out script

--- scripts/signout.js
+++ scripts/signout.js
@@ -2,7 +2,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (!user) {
         // No user is signed in, redirect to login page
         window.location.assign("/html/login.html");
-    } else if (user && window.location.href === "/html/index.html") {
+    } else if (user && window.location.href === "/index.html") {
         window.location.assign("/html/main.html");
     }
 });
@@ -10,7 +10,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 document.getElementById("logoutButton").addEventListener("click", function() {
     firebase.auth().signOut().then(() => {
         // Successfully signed out
-        window.location.assign("/html/index.html");
+        window.location.assign("/index.html");
     }).catch((error) => {
         console.error("Error signing out: ", error);
     });
